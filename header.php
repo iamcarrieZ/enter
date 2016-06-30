2c1e2006612ed566cd6f4b713451421b5c80aa63
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-06-30 11:22:01 +0800

    优化页头样式

--- header.php
+++ header.php
@@ -25,45 +25,44 @@
 <div id="<?= ( wp_is_mobile() ) ? 'page-mobile' : 'page'; ?>" class="site <?= ( wp_is_mobile() ) ? 'snap-content' : ''; ?>">
 
 	<header id="masthead" class="site-header" role="banner">
+
+		<?php if ( wp_is_mobile() ): ?>
+
+			<div class="mobile-nav clearfix">
+				<button id="open-left" class="toggle-button pull-left">☰</button>
+				<button id="open-right" class="toggle-button pull-right">☰</button>
+				<h1 class="site-title nav-brand"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			</div>
+
+		<?php else: ?>
+
 		<div class="wrap">
 			<div class="pure-g row">
-
-				<?php if ( wp_is_mobile() ): ?>
-
-					<div class="mobile-nav clearfix">
-						<button id="open-left" class="toggle-button pull-left">☰</button>
-						<button id="open-right" class="toggle-button pull-right">☰</button>
-						<h1 class="site-title pure-u"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<div class="pure-u-1 pure-u-md-1-4">
+					<div class="col">
+						<div class="site-branding pure-center-md">
+							<h1 class="site-title nav-brand"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						</div><!-- .site-branding -->
 					</div>
+				</div>
 
-				<?php else: ?>
-
-					<div class="pure-u-1 pure-u-md-1-4">
-						<div class="col">
-							<div class="site-branding pure-center-md">
-								<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-							</div><!-- .site-branding -->
-						</div>
+				<div class="pure-u-1 pure-u-md-3-4">
+					<div class="col">
+						<nav id="site-navigation" class="clearfix main-navigation" role="navigation">
+							<?php wp_nav_menu( [
+								'theme_location'  => 'primary',
+								'menu_id'         => 'primary-menu',
+								'container'       => 'div',
+								'container_class' => 'pull-right',
+								'menu_class'      => 'sf-menu',
+							] ); ?>
+						</nav><!-- #site-navigation -->
 					</div>
-
-					<div class="pure-u-1 pure-u-md-3-4">
-						<div class="col">
-							<nav id="site-navigation" class="clearfix main-navigation" role="navigation">
-								<?php wp_nav_menu( [
-									'theme_location'  => 'primary',
-									'menu_id'         => 'primary-menu',
-									'container'       => 'div',
-									'container_class' => 'pull-right',
-									'menu_class'      => 'sf-menu',
-								] ); ?>
-							</nav><!-- #site-navigation -->
-						</div>
-					</div>
-
-				<?php endif; ?>
-
+				</div>
 			</div>
 		</div>
+
+		<?php endif; ?>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
