d4c2e9587016831ddc868048e79d26abdb2d172b
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-06-30 08:58:05 +0800

    添加移动端页头页脚

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 
 <footer id="colophon" class="site-footer" role="contentinfo">
 
-	<div class="wrap site-info">
+	<div class="wrap site-info pure-center-md">
 		Copyright ©<?= date('Y'); ?> <a href="<?= home_url(); ?>"><?php bloginfo('name'); ?></a>
 	</div>
 
@@ -24,6 +24,68 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+
+<?php if ( wp_is_mobile() ): ?>
+
+    <div class="snap-drawers">
+
+        <nav id="mobile-navigation" class="main-navigation snap-drawer snap-drawer-left" role="navigation">
+            <?php wp_nav_menu( [
+                'theme_location'  => 'primary',
+                'menu_id'         => 'primary-menu',
+                'container'       => 'div',
+                'container_class' => 'mobile-menu',
+                'menu_class'      => 'sf-menu',
+            ] ); ?>
+        </nav>
+
+        <aside id="sidebar-mobile" class="mobile-widget-area snap-drawer snap-drawer-right" role="complementary">
+            <div class="col">
+                <?php dynamic_sidebar( 'sidebar-main' ); ?>
+            </div>
+        </aside>
+
+    </div>
+
+    <script>
+
+        var snapper = new Snap({
+            element: document.getElementById('page-mobile')
+        });
+
+        var addEvent = function addEvent(element, eventName, func) {
+            if (element.addEventListener) {
+                return element.addEventListener(eventName, func, false);
+            } else if (element.attachEvent) {
+                return element.attachEvent("on" + eventName, func);
+            }
+        };
+
+        addEvent(document.getElementById('open-left'), 'click', function () {
+            snapper.open('left');
+        });
+
+        addEvent(document.getElementById('open-right'), 'click', function () {
+            snapper.open('right');
+        });
+
+        /* 作为 APP 查看时, 阻止 Safari 打开链接 */
+        (function (a, b, c) {
+            if (c in b && b[c]) {
+                var d, e = a.location, f = /^(a|html)$/i;
+                a.addEventListener("click", function (a) {
+                    d = a.target;
+                    while (!f.test(d.nodeName)) {
+                        d = d.parentNode;
+                    }
+                    "href" in d && (d.href.indexOf("http") || ~d.href.indexOf(e.host)) && (a.preventDefault(), e.href = d.href)
+                }, !1)
+            }
+        })(document, window.navigator, "standalone");
+
+    </script>
+
+<?php endif; ?>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -21,36 +21,49 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'enter' ); ?></a>
+
+<div id="<?= ( wp_is_mobile() ) ? 'page-mobile' : 'page'; ?>" class="site <?= ( wp_is_mobile() ) ? 'snap-content' : ''; ?>">
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="wrap site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
+		<div class="wrap">
+			<div class="pure-g row">
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-				<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+				<?php if ( wp_is_mobile() ): ?>
+
+					<div class="mobile-nav clearfix">
+						<button id="open-left" class="toggle-button pull-left">☰</button>
+						<button id="open-right" class="toggle-button pull-right">☰</button>
+						<h1 class="site-title pure-u"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					</div>
+
+				<?php else: ?>
+
+					<div class="pure-u-1 pure-u-md-1-4">
+						<div class="col">
+							<div class="site-branding pure-center-md">
+								<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+							</div><!-- .site-branding -->
+						</div>
+					</div>
+
+					<div class="pure-u-1 pure-u-md-3-4">
+						<div class="col">
+							<nav id="site-navigation" class="clearfix main-navigation" role="navigation">
+								<?php wp_nav_menu( [
+									'theme_location'  => 'primary',
+									'menu_id'         => 'primary-menu',
+									'container'       => 'div',
+									'container_class' => 'pull-right',
+									'menu_class'      => 'sf-menu',
+								] ); ?>
+							</nav><!-- #site-navigation -->
+						</div>
+					</div>
+
+				<?php endif; ?>
+
+			</div>
+		</div>
 	</header><!-- #masthead -->
-
-	<nav id="site-navigation" class="main-navigation" role="navigation">
-		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wizhi-enter' ); ?></button>
-		<?php wp_nav_menu( [
-			'theme_location'  => 'primary',
-			'menu_id'         => 'primary-menu',
-			'container'       => 'div',
-			'container_class' => 'wrap',
-			'menu_class'      => 'sf-menu',
-		] ); ?>
-	</nav><!-- #site-navigation -->
 
 	<div id="content" class="site-content">
