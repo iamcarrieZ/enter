d97baed10f2be2ce47748d10511b0d20c8a824c4
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-08-09 22:01:15 +0800

    去掉 404 页面中不必要的内容

--- 404.php
+++ 404.php
@@ -23,46 +23,12 @@ get_header(); ?>
 
 						<div class="page-content">
 							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'enter' ); ?></p>
-
-							<?php
-							get_search_form();
-
-							the_widget( 'WP_Widget_Recent_Posts' );
-
-							// 只有网站有多个分类时显示分类目录
-							if ( enter_categorized_blog() ) :
-								?>
-
-								<div class="widget widget_categories">
-									<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'enter' ); ?></h2>
-									<ul>
-										<?php
-										wp_list_categories( [
-											'orderby'    => 'count',
-											'order'      => 'DESC',
-											'show_count' => 1,
-											'title_li'   => '',
-											'number'     => 10,
-										] );
-										?>
-									</ul>
-								</div>
-
-								<?php
-							endif;
-
-							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'enter' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-							the_widget( 'WP_Widget_Tag_Cloud' );
-							?>
-
-						</div><!-- .page-content -->
+						</div>
 
 					</section>
 
 				</main>
-			</div><!-- #primary -->
+			</div>
 
 		</div>
 	</div>
