a128d8a608ab646ce10963eaad6a9223169233d7
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-09-02 14:14:15 +0800

    添加 favicon

--- header.php
+++ header.php
@@ -24,6 +24,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="shortcut icon" href="<?php bloginfo( 'template_url' ); ?>/front/dist/images/favicon.png" type="image/png" />
 
 	<?php wp_head(); ?>
 </head>
